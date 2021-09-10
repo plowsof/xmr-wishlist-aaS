d17371eca1b32293f46a2e099faa4c5e8b3990bc
plowsof <77655812+plowsof@users.noreply.github.com>
2021-09-10 04:24:01 +0100

    Add files via upload

--- helper/qrlogos/qr-code-logo.py
+++ helper/qrlogos/qr-code-logo.py
@@ -1,51 +1,54 @@
 import qrcode
 
 import json
-
+import requests
 from PIL import Image
 
 
+json_url = "https://funding.monerujo.app/wishlist-data.json"
+
 #Thrown together, so it's all hardcoded 
 
-def wishlist_makeqrs(wishlist):
-    with open(wishlist) as json_file:
-        data = json.load(json_file)
-        for wish in data[0]:
-            desc = wish["desc"]
-            address = wish["address"]
-            print(wish["address"])
-
-            qr = qrcode.QRCode(
-                version=None,
-                error_correction=qrcode.constants.ERROR_CORRECT_M,
-                box_size=7,
-                border=4,
-            )
-
-            data = f"monero:{address}"
-
-            qr.add_data(data)
-
-            qr.make(fit=True)
-
-            img = qr.make_image(fill_color="black", back_color="white")
-            img.save(f"{desc}.png")
-            f_logo = "gunther2.png"
-            logo = Image.open(f_logo)
-            logo = logo.convert("RGBA")
-
-            print(logo.size)
-            im = Image.open(f"{desc}.png")
-            im = im.convert("RGBA")
-            logo.thumbnail((60, 60))
+def getJson():
+    global json_url
+    x = requests.get(json_url)
+    return json.loads(x.text)
 
 
-            #343 
-            #region = logo
-            #box = (135,135,235,235)
-            #region = region.resize((box[2] - box[0], box[3] - box[1]))
-            im.paste(logo, (142, 142))
-            #im.show()
-            im.save(f"{desc}.png")
+def wishlist_makeqrs():
+    saved_wishlist = getJson()
+    for wish in saved_wishlist[0]:
+        desc = wish["desc"]
+        address = wish["address"]
+        title = wish["title"]
+        print(wish["address"])
 
-wishlist_makeqrs("monerujo_wishlist.json")
+        qr = qrcode.QRCode(
+            version=None,
+            error_correction=qrcode.constants.ERROR_CORRECT_M,
+            box_size=7,
+            border=4,
+        )
+
+        data = f"monero:{address}"
+
+        qr.add_data(data)
+
+        qr.make(fit=True)
+
+        img = qr.make_image(fill_color="black", back_color="white")
+        img.save(f"{title}.png")
+        f_logo = "gunther2.png"
+        logo = Image.open(f_logo)
+        logo = logo.convert("RGBA")
+
+        print(logo.size)
+        im = Image.open(f"{title}.png")
+        im = im.convert("RGBA")
+        logo.thumbnail((60, 60))
+
+        im.paste(logo, (142, 142))
+        #im.show()
+        im.save(f"{title}.png")
+
+wishlist_makeqrs()
